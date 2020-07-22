f62699cae31d35a7b70ddfcb400c9318790d71bf
Juris Andersons <juris.andersons@emergen.com>
2020-07-22 22:39:11 +0300

    Updates

--- js/footer.js
+++ js/footer.js
@@ -3,8 +3,10 @@ export const footer = () => {
     //footerDiv.textContent = "No copyrights " + new Date().getFullYear();
     const footerDiv = document.querySelector("#main-footer");
     if (footerDiv != null) {
-        console.log("Found footer");
-        footerDiv.textContent = "jurisandersons.com";
+        // console.log("Found footer");
+        footerDiv.innerHTML =
+            "<a href='https://jurisandersons.com'>jurisandersons.com</a> | " +
+                new Date().getFullYear();
     }
 };
 export const menuItems = (current, pages) => {
